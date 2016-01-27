1ca0c8353e64fbacf89366a12a10ee70ceaee599
cmis <cmis@cmis-hs04.(none)>
2016-01-27 11:14:34 +0700

    I created a file.

---
+++ hello.py
@@ -0,0 +1,12 @@
+print "Hello, world"
+print "Who are you?"
+print "I'm Bomin"
+print "Why are you here?"
+print "HAHA."
+print True 
+print False
+print 16
+print 16.89999999999
+print "Bomin Keum"
+print ("Keum", "Bomin", "16", "Korean")
+print "NOT AN ERROR"
